4fdb9d13403ca320fddfbc10806db1262e296087
Nicola <ngrassini@yahoo.it>
2019-04-25 14:50:02 +0100

    Updating not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
     return(
-        <div><h1>The page does not exist.</h1></div>
+        <div>
+            <h1>The page does not exist.</h1>
+            <Link className='a1' to='/'>Back to home page</Link>
+        </div>
+        
     );
 }
+
