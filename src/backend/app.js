14863ff9b246842bbea1fdc7b78785459db415ee
Nicola <ngrassini@yahoo.it>
2019-05-03 12:12:40 +0100

    json changes

--- src/backend/app.js
+++ src/backend/app.js
@@ -9,7 +9,7 @@ const getShows = require('./shows');
 
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
