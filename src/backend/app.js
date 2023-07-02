fecfc2b464b74a683974173eb4ceba89f10ff7da
Nicola <ngrassini@yahoo.it>
2023-07-03 00:10:06 +0100

    add react routing related comment to app.js

--- src/backend/app.js
+++ src/backend/app.js
@@ -26,10 +26,11 @@ db.connect().then(dbo => {
         });
     });
 
-    //serve static files
+    // serve static files
     app.use(express.static(path.join(__dirname, '../../build')));
 
-    app.get('*', function (req, res) {
+    // serve index.html on all routes - strategy to make reloads work on all (not "/") routes with react 'BrowserRouter'
+    app.get('*', function (_req, res) {
         res.sendFile(path.join(__dirname, '../../build', 'index.html'));
     });
     
